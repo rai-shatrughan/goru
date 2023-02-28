6c32cc833b6de3ae67ff1d7490759c56ac8d2e78
sr <sr@debian-BULLSEYE-live-builder-AMD64>
2023-02-28 15:53:31 +0530

    inp

--- gsvc/exp/pwauto/main.go
+++ gsvc/exp/pwauto/main.go
@@ -7,31 +7,49 @@ import (
 	"github.com/playwright-community/playwright-go"
 )
 
-func init() {
-	err := playwright.Install()
-	if err != nil {
-		log.Fatalf("could not install playwright: %v", err)
-	}
-}
+var (
+	browser playwright.Browser
+	page    playwright.Page
+	pw      *playwright.Playwright
+	err     error
+)
 
 func main() {
+	setup()
+	defer teardown()
 	gnews()
 	cnn()
 }
 
-func cnn() {
-	pw, err := playwright.Run()
+func setup() {
+	err = playwright.Install()
+	if err != nil {
+		log.Fatalf("could not install playwright: %v", err)
+	}
+	pw, err = playwright.Run()
 	if err != nil {
 		log.Fatalf("could not start playwright: %v", err)
 	}
-	browser, err := pw.Chromium.Launch()
+	browser, err = pw.Chromium.Launch()
 	if err != nil {
 		log.Fatalf("could not launch browser: %v", err)
 	}
-	page, err := browser.NewPage()
+	page, err = browser.NewPage()
 	if err != nil {
 		log.Fatalf("could not create page: %v", err)
 	}
+}
+
+func teardown() {
+	if err := browser.Close(); err != nil {
+		log.Fatalf("could not close browser: %v", err)
+	}
+	if err := pw.Stop(); err != nil {
+		log.Fatalf("could not stop Playwright: %v", err)
+	}
+}
+
+func cnn() {
 	if _, err = page.Goto("https://edition.cnn.com/"); err != nil {
 		log.Fatalf("could not goto: %v", err)
 	}
@@ -46,28 +64,11 @@ func cnn() {
 		}
 		fmt.Printf("%d: %s\n", i+1, titleElement)
 	}
-	if err = browser.Close(); err != nil {
-		log.Fatalf("could not close browser: %v", err)
-	}
-	if err = pw.Stop(); err != nil {
-		log.Fatalf("could not stop Playwright: %v", err)
-	}
+
 }
 
 func gnews() {
-	pw, err := playwright.Run()
-	if err != nil {
-		log.Fatalf("could not start playwright: %v", err)
-	}
-	browser, err := pw.Chromium.Launch()
-	if err != nil {
-		log.Fatalf("could not launch browser: %v", err)
-	}
-	page, err := browser.NewPage()
-	if err != nil {
-		log.Fatalf("could not create page: %v", err)
-	}
-	if _, err = page.Goto("https://news.google.co.in"); err != nil {
+	if _, err := page.Goto("https://news.google.co.in"); err != nil {
 		log.Fatalf("could not goto: %v", err)
 	}
 	entries, err := page.QuerySelectorAll("xpath=//article//a")
@@ -80,11 +81,5 @@ func gnews() {
 			log.Fatalf("could not get title element: %v", err)
 		}
 		fmt.Printf("%d: %s\n", i+1, titleElement)
-	}
-	if err = browser.Close(); err != nil {
-		log.Fatalf("could not close browser: %v", err)
-	}
-	if err = pw.Stop(); err != nil {
-		log.Fatalf("could not stop Playwright: %v", err)
 	}
 }
